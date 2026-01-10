0e50bf5347f934b5d0e9200b991ca9988f006e42
Ramon Ledesma <rmon.led@gmail.com>
2026-01-10 15:46:17 -0800

    example
    Co-authored-by: contessa09 <contessa09@users.noreply.github.com>
    Co-authored-by: Gamerminion <Gamerminion@users.noreply.github.com>
    Co-authored-by: vienphitran1107-blip <vienphitran1107-blip@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,22 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
 
 public class RobotContainer {
+
+  private CommandXboxController driver = new CommandXboxController(0);
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+
+    driver.a().onTrue(Commands.print("Program me!"));
+
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
